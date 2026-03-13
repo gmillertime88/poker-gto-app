b4e005207ee707e737c9889b07583a9ff98a1b0f
gmillertime88 <gmiller@proactivetech.com>
2026-03-13 10:43:14 -0400

    Update app

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const TABLE_TEMPERATURES = [
   { key: "aggressive", label: "Aggressive" },
 ];
 
-const BUILD_VERSION = "3.4";
-const BUILD_TIMESTAMP = "2026-03-13 10:35";
+const BUILD_VERSION = "3.5";
+const BUILD_TIMESTAMP = "2026-03-13 10:43";
 
 const POSITION_DISPLAY_ORDER = ["D", "SB", "BB", "UTG", "MP1", "MP2", "MP3", "HJ", "CO"];
 
