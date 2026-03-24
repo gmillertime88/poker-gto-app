69619bc4d6e1b1af30a00225634d56871cbd6bef
gmillertime88 <gmiller@proactivetech.com>
2026-03-24 13:20:33 -0400

    Update app

--- odds.js
+++ odds.js
@@ -13,8 +13,8 @@ const SUITS = [
   { key: "C", symbol: "♣", label: "Clubs", colorClass: "suit-black" },
 ];
 
-const BUILD_VERSION = "6.8";
-const BUILD_TIMESTAMP = "2026-03-24 09:42";
+const BUILD_VERSION = "6.9";
+const BUILD_TIMESTAMP = "2026-03-24 13:20";
 const WHEEL_REPEAT_COUNT = 3;
 const WHEEL_SCROLL_DEBOUNCE_MS = 90;
 
